fc99ae8138404604b7d7554d6c6994bcac9643ac
Arca Arslan <126979156+Semih993@users.noreply.github.com>
2026-02-15 21:09:56 +0000

    Update telegram_chat_ID.py
    Enter your telegram token in the 'TELEGRAM_TOKEN' variable

--- telegram_chat_ID.py
+++ telegram_chat_ID.py
@@ -1,6 +1,7 @@
 import requests
 
-TELEGRAM_TOKEN = "7605615062:AAEPIqm0ZmhUJ7o1S-GgPiK1WSRikgwmhKM"
+TELEGRAM_TOKEN = "" # Enter telegram token here
 url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getUpdates"
 response = requests.get(url)
 print(response.json())
+
